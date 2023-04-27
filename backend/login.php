68ac9d11bd574d72054b964e21145e89854e1241
Mukhtar Sani <mukhtarsani20@gmail.com>
2023-04-27 10:47:07 +0800

    Update login.php
    handle the error better

--- backend/login.php
+++ backend/login.php
@@ -9,48 +9,47 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Connect to the database
-  $conn = new mysqli('localhost', 'username', 'password', 'database_name');
+  try {
+    // Connect to the database
+    $conn = new mysqli('localhost', 'username', 'password', 'database_name');
 
-  // Check for connection errors
-  if ($conn->connect_error) {
-    echo json_encode([
-      'success' => false,
-      'message' => 'Database connection error'
-    ]);
-    exit();
-  }
+    // Prepare the query
+    $stmt = $conn->prepare('SELECT * FROM users WHERE username = ?');
 
-  // Prepare the query
-  $stmt = $conn->prepare('SELECT * FROM users WHERE username = ?');
+    // Bind the username parameter
+    $stmt->bind_param('s', $username);
 
-  // Bind the username parameter
-  $stmt->bind_param('s', $username);
+    // Execute the query
+    $stmt->execute();
 
-  // Execute the query
-  $stmt->execute();
+    // Get the query result
+    $result = $stmt->get_result();
 
-  // Get the query result
-  $result = $stmt->get_result();
+    // Check if the username exists in the database
+    if ($result->num_rows === 1) {
 
-  // Check if the username exists in the database
-  if ($result->num_rows === 1) {
+      // Fetch the user data
+      $row = $result->fetch_assoc();
 
-    // Fetch the user data
-    $row = $result->fetch_assoc();
+      // Verify the password
+      if (password_verify($password, $row['password'])) {
 
-    // Verify the password
-    if (password_verify($password, $row['password'])) {
+        // Set the user session
+        $_SESSION['user'] = $username;
 
-      // Set the user session
-      $_SESSION['user'] = $username;
+        // Return a success JSON response
+        echo json_encode([
+          'success' => true,
+          'message' => 'Login successful'
+        ]);
+      } else {
 
-      // Return a success JSON response
-      echo json_encode([
-        'success' => true,
-        'message' => 'Login successful'
-      ]);
-
+        // Return an error JSON response
+        echo json_encode([
+          'success' => false,
+          'message' => 'Invalid username or password'
+        ]);
+      }
     } else {
 
       // Return an error JSON response
@@ -58,30 +57,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'success' => false,
         'message' => 'Invalid username or password'
       ]);
-
     }
 
-  } else {
-
-    // Return an error JSON response
+    // Close the statement and database connection
+    $stmt->close();
+    $conn->close();
+  } catch (Exception $e) {
+    // Return a database connection error JSON response
     echo json_encode([
       'success' => false,
-      'message' => 'Invalid username or password'
+      'message' => 'Database connection error'
     ]);
-
+    exit();
   }
-
-  // Close the statement and database connection
-  $stmt->close();
-  $conn->close();
-
 } else {
-  
+
   // Return a bad request JSON response
   http_response_code(400);
   echo json_encode([
     'success' => false,
     'message' => 'Bad request'
   ]);
-
 }
