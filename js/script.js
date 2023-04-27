049743c1bcadeb9c5ce6c3d53b289f8832849559
Mukhtar Sani <mukhtarsani20@gmail.com>
2023-04-27 10:19:29 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+<script>
+  // Function to send login request
+  function login() {
+    // Get form data
+    var form = document.getElementById("login-form");
+    var formData = new FormData(form);
+
+    // Send login request
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        var response = JSON.parse(xhr.responseText);
+        if (xhr.status === 200) {
+          // Success
+          document.getElementById("login-message").innerHTML = "<div class='alert alert-success' role='alert'>" + response.message + "</div>";
+        } else {
+          // Error
+          document.getElementById("login-message").innerHTML = "<div class='alert alert-danger' role='alert'>" + response.message + "</div>";
+        }
+      }
+    };
+    xhr.open("POST", "http://localhost/Basic-PHP-Login-System/login.php", true);
+    xhr.send(formData);
+  }
+
+  // Form validation
+  var form = document.getElementById("login-form");
+  form.addEventListener("submit", function(event) {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add("was-validated");
+    login();
+  });
+</script>
